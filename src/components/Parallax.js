88b57a29ccb6e8d8ff250be46f05df938cead08d
Tom Officer <thomasofficerjr@gmail.com>
2023-07-26 01:38:31 -0400

    started adding animations to parallax logo

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,8 +1,18 @@
 import React, { useEffect } from 'react';
 import '../componentStyling/parallax.css';
-import { Image, Box, Flex } from '@chakra-ui/react';
+import { Image, Box, Flex, chakra, shouldForwardProp } from '@chakra-ui/react';
+import { motion, isValidMotionProp } from 'framer-motion';
 
 const Parallax = () => {
+  //motion animations
+  const ChakraBox = chakra(motion.div, {
+    /**
+     * Allow motion props and non-Chakra props to be forwarded.
+     */
+    shouldForwardProp: prop =>
+      isValidMotionProp(prop) || shouldForwardProp(prop),
+  });
+
   useEffect(() => {
     const handleScroll = () => {
       const parallaxLayers = document.querySelectorAll('.parallax-layer');
@@ -34,12 +44,28 @@ const Parallax = () => {
 
           <div className="parallax-layer" data-speed="0.5">
             <Flex h="100vh" alignItems="center" justifyContent="center">
-              <Image
-                maxW={'xl'}
-                opacity={'75%'}
-                src="https://ik.imagekit.io/v66nb6oaq/whitedogblack_Je04hqCmN.png?updatedAt=1661297820983"
-                alt="logo"
-              />
+              <ChakraBox
+                animate={{
+                  rotate: [0, 360], // Rotate from 0 degrees to 360 degrees
+                  transition: {
+                    duration: 1.5, // Duration of the animation in seconds
+                    ease: 'linear', // Type of easing function (optional)
+                  },
+                }}
+                transition={{
+                  duration: 3,
+                  ease: 'easeInOut',
+                  repeat: Infinity,
+                  repeatType: 'loop',
+                }}
+              >
+                <Image
+                  maxW={'xl'}
+                  opacity={'75%'}
+                  src="https://ik.imagekit.io/v66nb6oaq/whitedogblack_Je04hqCmN.png?updatedAt=1661297820983"
+                  alt="logo"
+                />
+              </ChakraBox>
             </Flex>
           </div>
         </div>
