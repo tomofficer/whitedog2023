1910a454db898009bb5f9a017328a977a570eb97
Tom Officer <thomasofficerjr@gmail.com>
2023-07-28 23:53:25 -0400

    added functioning gallery component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import Header from './components/Header';
 import Parallax from './components/Parallax';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import Gallery2 from './components/Gallery2';
 import Contact from './components/Contact';
-import Testing from './components/Testing';
-import Testing2 from './components/Testing2';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Header />
-        {/* <Hero /> */}
         <Parallax />
         <About />
-        <Gallery />
-        {/* <Testing /> */}
-        <Testing2 />
+        {/* <Gallery /> */}
+        <Gallery2 />
         <Contact />
       </Box>
     </ChakraProvider>
---
+++ src/components/Gallery2.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import { Box, Grid, Heading } from '@chakra-ui/react';
+
+const ImageGallery = () => {
+  //state
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(null);
+
+  //image data
+  const galleryData = [
+    {
+      image:
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/whitedog1_-DJZlIxPp.jpg?updatedAt=1690484185418',
+      relatedImages: [
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/patrick-robert-doyle-OvXht_wi5Ew-unsplash_dEhch8ueo.jpg?updatedAt=1690600655926',
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/davide-cantelli-e3Uy4k7ooYk-unsplash_qaQTgUjXW.jpg?updatedAt=1690600645524',
+      ],
+    },
+    {
+      image:
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/frames-for-your-heart-MyeOnGcibCQ-unsplash_lTu8kwUQw.jpg?updatedAt=1690600604766',
+      relatedImages: [
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/inaki-del-olmo-NIJuEQw0RKg-unsplash_-6ZbpxdoU.jpg?updatedAt=1690600636071',
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/aaron-huber-G7sE2S4Lab4-unsplash_pYXqHhePm.jpg?updatedAt=1690600592957',
+      ],
+    },
+    {
+      image:
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/hutomo-abrianto-9ZvuWg8deho-unsplash_xlgzVzHF0.jpg?updatedAt=1690600580601',
+      relatedImages: [
+        'relhttps://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/lauren-mancke-rm5DbquteoY-unsplash_IqgME0dtk.jpg?updatedAt=1690600557169ated_image_url_5',
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/zac-gudakov-5QLCohwVndQ-unsplash_e_Z9iX5bz.jpg?updatedAt=1690600750853',
+      ],
+    },
+    {
+      image:
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/whitedog1_-DJZlIxPp.jpg?updatedAt=1690484185418',
+      relatedImages: [
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/patrick-robert-doyle-OvXht_wi5Ew-unsplash_dEhch8ueo.jpg?updatedAt=1690600655926',
+        'https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/davide-cantelli-e3Uy4k7ooYk-unsplash_qaQTgUjXW.jpg?updatedAt=1690600645524',
+      ],
+    },
+  ];
+
+  //modal handlers
+  const openModal = index => {
+    setSelectedImageIndex(index);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <Box
+        py={{ base: 4, sm: 0, md: 4 }}
+        mt={{ base: 2, sm: 6, md: 28 }}
+        px={{ base: 10, sm: 20, md: 36 }}
+      >
+        <Heading>Gallery</Heading>
+        <Grid
+          templateColumns="repeat(2, 1fr)"
+          gap={20}
+          my={{ base: 6, sm: 10, md: 24 }}
+        >
+          {galleryData.map((data, index) => (
+            <img
+              key={index}
+              src={data.image}
+              alt={`Image ${index + 1}`}
+              onClick={() => openModal(index)}
+            />
+          ))}
+        </Grid>
+      </Box>
+
+      <Modal isOpen={isOpen} onRequestClose={closeModal} className="modal">
+        {selectedImageIndex !== null && (
+          <div>
+            <img
+              src={galleryData[selectedImageIndex].image}
+              alt="Selected Image"
+            />
+            <div className="related-images">
+              {galleryData[selectedImageIndex].relatedImages.map(
+                (image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Related Image ${index + 1}`}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+};
+
+export default ImageGallery;
