c1bbdf959896928e2d5a6823533d5b3a4c58c0c4
Tom Officer <thomasofficerjr@gmail.com>
2023-07-29 23:22:05 -0400

    updated contact footer styling, updated scroll effects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,12 +19,17 @@ const Contact = ({ contactRef }) => {
   return (
     <>
       <Center>
-        <Box py={{ base: 4, sm: 0, md: 0 }} mt={{ base: 2, sm: 6, md: 20 }}>
-          <Box bg="white" py={{ base: 4, sm: 0, md: 10 }}>
+        <Box
+          py={{ base: 4, sm: 0, md: 0 }}
+          px={{ base: 4, sm: 0, md: 20 }}
+          mt={{ base: 2, sm: 6, md: 20 }}
+          mb={{ base: 2, sm: 6, md: 14 }}
+        >
+          <Box bg="white" py={{ base: 4, sm: 0, md: 0 }}>
             <Grid
               templateColumns="repeat(3, 1fr)"
               gap={20}
-              my={{ base: 6, sm: 10, md: 24 }}
+              mt={{ base: 6, sm: 10, md: 24 }}
             >
               <Flex direction="column" justifyContent={'center'}>
                 <div ref={contactRef}></div>
@@ -57,7 +62,7 @@ const Contact = ({ contactRef }) => {
               </Flex>
               <Image
                 mx={10}
-                maxW="sm"
+                maxW="350px"
                 src="https://ik.imagekit.io/v66nb6oaq/White%20Dog%20Woodworking/whitedogblack_Je04hqCmN.png?updatedAt=1690484225404"
               />
               <ContactForm />
